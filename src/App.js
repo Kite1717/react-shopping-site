10498fa65f9034a29b0d16b33ae94f23763da4a8
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-06-10 14:19:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 
-
-import {Navbar,ProductsRenderer} from './components'
+import {Router} from './util'
 
  class App extends Component {
   render() {
     return (
-      <div className = "container" >
-        <Navbar/>
-        <ProductsRenderer/>
-      </div>
+      <Router/>
     )
   }
 }
